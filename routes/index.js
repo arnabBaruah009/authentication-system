d81515d09e9247d44e9615aeb94926a512bb6d10
Arnab Baruah <baruaharnab769@gmail.com>
2023-04-03 17:55:48 +0530

    Required forgot_password routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
 router.use('/user', require('./user'));
+router.use('/forgot_password', require('./forgot_password'));
 
 console.log('Router is working');
 
